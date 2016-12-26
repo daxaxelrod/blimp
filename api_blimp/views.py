5075dffefbf1b3d5c04814b6200c695649da702d
David Axelrod <daxaxelrod@gmail.com>
2016-12-26 01:27:07 -0500

    welp learned something about cbv method decorators

--- api_blimp/views.py
+++ api_blimp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework import generics, mixins, permissions, viewsets, status
@@ -26,15 +27,12 @@ class GetUpdateGame(generics.RetrieveUpdateAPIView):
     serializer_class = serializers.GameSerializer
     lookup_url_kwarg = "game_pk"
 
-
-
+@method_decorator(csrf_exempt, name='dispatch')
 class GameSearcher(View):
 
     http_method_names = ['post']
-
-    @csrf_exempt
+    
     def post(self, request, format=None):
-
         # Serialize "new" member's email
         serializer = serializers.SearchingPlayerSerializer(data=request.DATA)
 
